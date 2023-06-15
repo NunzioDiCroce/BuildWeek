1c391d561356d3ed6127357245029b30e54cc94b
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-15 11:17:56 +0200

    commit

--- Angular/BW3/src/app/app.module.ts
+++ Angular/BW3/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import Route, RouterModule per gestione rotte
+import { Route, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
