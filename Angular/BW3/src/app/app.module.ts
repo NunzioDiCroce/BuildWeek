00afb3acd91ac66cc3941923f7c2884e4181842a
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-15 11:28:14 +0200

    commit

--- Angular/BW3/src/app/app.module.ts
+++ Angular/BW3/src/app/app.module.ts
@@ -6,11 +6,25 @@ import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    PostsComponent,
+    CommentsComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    TodosComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
